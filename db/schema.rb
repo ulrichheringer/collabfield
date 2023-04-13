d468e0ae86eab63ca06d1b5530b96e3c47e5f08b
phdevbr <pedroheringer2020@gmail.com>
2023-04-13 16:52:08 -0300

    added name field to devise user

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_13_192220) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
+    t.string "name", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
