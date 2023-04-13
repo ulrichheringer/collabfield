365a3247044acfa91530f31781921cac2c394037
phdevbr <pedroheringer2020@gmail.com>
2023-04-13 17:09:48 -0300

    changed users/sign_up route to signup/

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
+    get 'signup', to: 'devise/registrations#new'
   end
   root to: 'pages#index'
 end
